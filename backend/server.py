c2812dd612f81f5b3e317ffa330bff020f70b82c
dmojzes <imdanielmojzes@gmail.com>
2025-09-20 19:39:52 -0400

    Further improvement comments

--- backend/server.py
+++ backend/server.py
@@ -6,6 +6,11 @@ CORS(app)
 
 @app.route("/api/find", methods=["POST"])
 def find_side_effects():
+    # data is a string value representing the user inputted medication name
+    # it should be passed to the database to make sure its valid
+    # if valid, pass it to the model and retrieve some value
+    # ensure that the retrieved value is in json format before returning it
+    # once returned, the frontend should format the data in a way that looks good to the user
     data = request.get_json()
     query = data.get("query", "")
     return jsonify({"result": f"You searched for: {query}"})
